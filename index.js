02db50a91b14766d3179982bc8211cb564102f2e
Owlz93 <k.mcilroy.wilkinson@gmail.com>
2022-10-28 14:12:57 +0100

    Indexupdate

--- index.js
+++ index.js
@@ -1,20 +1,49 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
+const fs = require('node:fs');
+const path = require('node:path');
 
-const client = new Discord.Client({intents: ["GUILDS", "GUILD_MESSAGES"]});
 
-client.login(config.BOT_TOKEN);
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 // When the client is ready, run this code (only once)
-client.once('ready', () => {
-	console.log('Ready!');
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready to gooo! Logged in as ${c.user.tag}`);
 });
 
-client.on('message', message => {
-    if (message.content === 'I AM A MOUNTAIN') {
-        message.channel.send('You were a boulder!');
-    }
+/*-----listen to commands-----*/
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+client.once(Events.ClientReady, () => {
+	console.log('Ready to listen to commands!');
 });
 
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+/*----------*/
+
+// Log in to Discord with your client's token
+client.login("MTAzNDA0OTEzODg5NzIwNzI5Ng.GDnbji.aQ1Hdu_JfqABKzxB1GwjMen36rC_BArxo6fd2c");
