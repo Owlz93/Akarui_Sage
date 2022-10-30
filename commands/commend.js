368932a9f9a8019e1a98921c14e496d6949e934e
AKARUI <jana.devoecht@gmail.com>
2022-10-30 17:39:33 +0100

    Added comment of agent commends

--- commands/commend.js
+++ commands/commend.js
@@ -1,5 +1,32 @@
 const { SlashCommandBuilder } = require('discord.js');
 
+
+//Commend options for all agents
+
+/*
+Astra: Nice Nice
+Breach: Hell Of A Job
+Brimstone: GoodWork
+Chamber: Bravo
+Cypher: Well done
+Fade: Not Bad
+Jett: GoodWork
+Kay0: SolidWork
+KillJoy: You Did Well
+Neon: Nice Job
+Omen: Well done
+Phoenix: Nice
+Raze: Hey Nice Work
+Reyna: Impressive
+Sage: Wonderful
+Skye: GoodOnYa
+Sova: Good
+Viper: I'm... Impressed
+Yoru: Not Bad
+Harbor: Hah, Well done
+*/
+
+
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('commend')
