b990136b1bfa7383c6ff2db0cce5e9b8a1e1c9e5
Owlz93 <k.mcilroy.wilkinson@gmail.com>
2022-10-30 15:45:46 +0000

    Introducing role

---
+++ commands/ReactionRole.js
@@ -0,0 +1,63 @@
+module.exports = {
+    name: 'reactionrole',
+    description: "Sets up a reaction role message!",
+    async execute(message, args, Discord, client) {
+        const channel = '1036302764965363773';
+        const yellowTeamRole = message.guild.roles.cache.find(role => role.name === "EYO");
+        const blueTeamRole = message.guild.roles.cache.find(role => role.name === "Sus");
+
+        const eyoTeamEmoji = ':eyes:';
+        const susTeamEmoji = ':face_with_raised_eyebrow:';
+
+        let embed = new Discord.MessageEmbed()
+            .setColor('#e42643')
+            .setTitle('Choose a team to play on!')
+            .setDescription('Choosing a team will allow you to interact with your teammates!\n\n'
+                + `${eyoTeamEmoji} for EYO team\n`
+                + `${susTeamEmoji} for Sus team`);
+
+        let messageEmbed = await message.channel.send(embed);
+        messageEmbed.react(eyoTeamEmoji);
+        messageEmbed.react(susTeamEmoji);
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === eyoTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(eyoTeamRole);
+                }
+                if (reaction.emoji.name === susTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.add(susTeamRole);
+                }
+            } else {
+                return;
+            }
+
+        });
+
+        client.on('messageReactionRemove', async (reaction, user) => {
+
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+            if (!reaction.message.guild) return;
+
+
+            if (reaction.message.channel.id == channel) {
+                if (reaction.emoji.name === eyoTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(eyoTeamRole);
+                }
+                if (reaction.emoji.name === susTeamEmoji) {
+                    await reaction.message.guild.members.cache.get(user.id).roles.remove(susTeamRole);
+                }
+            } else {
+                return;
+            }
+        });
+    }
+
+}
