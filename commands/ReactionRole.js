2ce4055fc421d7651ec6533964aa0f7f2fb53276
Owlz93 <k.mcilroy.wilkinson@gmail.com>
2022-10-31 17:26:13 +0000

    Eye emoji

--- commands/ReactionRole.js
+++ commands/ReactionRole.js
@@ -20,7 +20,11 @@ module.exports = {
             + `${eyeTeamEmoji} for Eyes team\n`
             + `${browTeamEmoji} for Eyebrow team`);
 
-    let messageEmbed = await message.channel.send({embeds: [embedExample] })
+    let messageEmbed = await message.channel.send({embeds: [embedExample] }).then(embedMessage => {
+    embedMessage.react(eyeTeamEmoji)
+	});
+
+
 		message.react('eyeTeamEmoji')
 				.then(() => message.react('browTeamEmoji'))
 				.catch(error => console.error('One of the emojis failed to react', error));
