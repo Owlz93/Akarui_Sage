c12bf35d32d4a57c448df601234601c7983ae482
Owlz93 <k.mcilroy.wilkinson@gmail.com>
2022-10-31 21:13:09 +0000

    Trying const client

--- commands/ReactionRole.js
+++ commands/ReactionRole.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
+const client = new Discord.Client();
 
 module.exports = {
 	data: new SlashCommandBuilder()
