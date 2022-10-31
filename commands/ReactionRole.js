1cc49e47660cdb7f1c55ef9424e8d6ece299805d
Owlz93 <k.mcilroy.wilkinson@gmail.com>
2022-10-31 20:17:14 +0000

    await

--- commands/ReactionRole.js
+++ commands/ReactionRole.js
@@ -27,7 +27,7 @@ module.exports = {
 			messageEmbed.react(browTeamEmoji),
 		])
 
-    client.on("messageReactionAdd", async (messageReaction, user){
+    client.on("messageReactionAdd", await(messageReaction, user){
 			console.log(`a reaction is added to a message`);
 		});
 
